e2c8e174d0093b27838d6e3a61c5bdff14daa07c
imamdnl <imamdnl11@gmail.com>
2019-11-07 22:51:07 +0700

    change base_url config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/gentelella/';
+$config['base_url'] = 'http://localhost/myDefaultCms-gentelella/';
 
 /*
 |--------------------------------------------------------------------------
