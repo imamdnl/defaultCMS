927074ba2c0a949bf56aaa5755bed4f99529aa2c
imamdnl <imamdnl11@gmail.com>
2019-11-07 22:20:24 +0700

    init default CMS using gentelella and CodeIgniter v3.1.11

---
+++ application/views/templates/header.php
@@ -0,0 +1,50 @@
+    <!-- Bootstrap -->
+    <link href="<?= base_url('assets/'); ?>vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="<?= base_url('assets/'); ?>vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="<?= base_url('assets/'); ?>vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- FullCalendar -->
+    <link href="<?= base_url('assets/'); ?>vendors/fullcalendar/dist/fullcalendar.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/fullcalendar/dist/fullcalendar.print.css" rel="stylesheet" media="print">
+    <!-- iCheck -->
+    <link href="<?= base_url('assets/'); ?>vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- bootstrap-daterangepicker -->
+    <link href="<?= base_url('assets/'); ?>vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <!-- bootstrap-datetimepicker -->
+    <link href="<?= base_url('assets/'); ?>vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+    <!-- Ion.RangeSlider -->
+    <link href="<?= base_url('assets/'); ?>vendors/normalize-css/normalize.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/ion.rangeSlider/css/ion.rangeSlider.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css" rel="stylesheet">
+    <!-- Bootstrap Colorpicker -->
+    <link href="<?= base_url('assets/'); ?>vendors/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css" rel="stylesheet">
+
+    <link href="<?= base_url('assets/'); ?>vendors/cropper/dist/cropper.min.css" rel="stylesheet">
+    <!-- Dropzone.js -->
+    <link href="<?= base_url('assets/'); ?>vendors/dropzone/dist/min/dropzone.min.css" rel="stylesheet">
+    <!-- bootstrap-wysiwyg -->
+    <link href="<?= base_url('assets/'); ?>vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+    <!-- Select2 -->
+    <link href="<?= base_url('assets/'); ?>vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+    <!-- Switchery -->
+    <link href="<?= base_url('assets/'); ?>vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+    <!-- starrr -->
+    <link href="<?= base_url('assets/'); ?>vendors/starrr/dist/starrr.css" rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="<?= base_url('assets/'); ?>vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- PNotify -->
+    <link href="<?= base_url('assets/'); ?>vendors/pnotify/dist/pnotify.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/pnotify/dist/pnotify.buttons.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/pnotify/dist/pnotify.nonblock.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="<?= base_url('assets/'); ?>vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+    <!-- Datatables -->
+    <link href="<?= base_url('assets/'); ?>vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="<?= base_url('assets/'); ?>build/css/custom.css" rel="stylesheet">
